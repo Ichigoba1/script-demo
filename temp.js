6096afbdf3972eb785a044e43adbbf99ee7b1cf5
Ichigoba1 <958229683@qq.com>
2022-08-22 19:54:16 +0800

    create temp.js

---
+++ temp.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         hello world
+// @namespace    https://ichigoba1.github.io/
+// @version      0.1
+// @description  new script
+// @author       1chigo
+// @match        *://*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// ==/UserScript==
+
+(
+    function() {
+    'use strict';
+
+    console.log("hello world");
+    }
+)();
